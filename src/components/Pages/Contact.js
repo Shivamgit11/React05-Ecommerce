5bc2f557eb07b4a24d1376c2d50daeeab99a1442
shivam <mailtoshivam2002@gmail.com>
2022-11-11 22:17:54 +0530

    Contact Us Page working

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,87 +1,49 @@
-// import classes from "./Contact.module.css";
-
-// const Contact = () => {
-//   async function SubmitHandler(check) {
-
-//     const response = await fetch(
-//       "https://contact01-bceab-default-rtdb.firebaseio.com",
-//       {
-//         method: "POST",
-
-//         headers: {
-//           "Content-Type": "application/json",
-//         },
-//       }
-//     );
-//     const data = await response.json();
-//     console.log(data);
-//   }
-//   return (
-//     <section className={classes.section1}>
-//       <h1>Contact US</h1>
-//       <label>Name</label>
-//       <input type="text" required />
-//       <label>Email</label>
-//       <input type="email" required />
-//       <label>Phone</label>
-//       <input type="text" required />
-//       <button onClick={SubmitHandler}>Submit</button>
-//     </section>
-//   );
-// };
-
-// export default Contact;
-
-import React, { Fragment, useRef } from "react";
-import classes from './Contact.module.css';
+import React, { useRef } from "react";
+import classes from "./Contact.module.css";
 
 const Contact = (props) => {
-  const usernameRef = useRef("");
-  const phoneRef = useRef("");
-  const emailRef = useRef("");
-  async function submitHandler(event) {
-    event.preventDefault();
-    const contact = {
-      name: usernameRef.current.value,
-      email: emailRef.current.value,
+  const usernameRef = useRef();
+  const phoneRef = useRef();
+  const emailRef = useRef();
+
+  async function SubmitHandler(check) {
+    check.preventDefault();
+    console.log(check);
+
+    const check2 = {
+      name:  usernameRef.current.value,
       phone: phoneRef.current.value,
-    };
-    console.log(contact);
+      Email: emailRef.current.value,
+    }
+    
 
     const response = await fetch(
-      "https://contact01-bceab-default-rtdb.firebaseio.com//contact.json",
+      "https://contact01-bceab-default-rtdb.firebaseio.com/check.json",
       {
         method: "POST",
-        body: JSON.stringify(contact),
+        body: JSON.stringify(check2),
         headers: {
-          "Content-type": "application/json",
+          "Content-Type": "application/json",
         },
       }
     );
     const data = await response.json();
     console.log(data);
   }
-
   return (
-    <Fragment>
-      <h1 className={classes.h1}>Connect with us</h1>
-      <form onSubmit={submitHandler} className={classes.section1}>
-        <label> Name</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          ref={usernameRef}
-          required
-        />
-        <label> EmailId</label>
-        <input id="email" type="email" name="emailId" ref={emailRef} required />
-        <label> Phone Number</label>
-        <input id="phonenumber" type="tel" name="phonenumber" ref={phoneRef} />
-        <button>Submit</button>
-      </form>
-    </Fragment>
+    <section className={classes.section1}>
+      <h1>Contact US</h1>
+      <label>Name</label>
+      <input  type="text" ref={usernameRef} required />
+      <label>Email</label>
+      <input type="email" ref={emailRef} required />
+      <label>Phone</label>
+      <input  type="tel"  ref={phoneRef} required />
+      <button onClick={SubmitHandler}>Submit</button>
+    </section>
   );
 };
 
 export default Contact;
+
+
